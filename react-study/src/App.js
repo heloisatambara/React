0de3617482e3490a472f986a0adf1cc2f8a2a907
Heloisa Tambara <heloisatambara@usp.br>
2023-02-16 17:01:59 -0300

    states

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -3,15 +3,46 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+  /* constructor(props) {
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this); // so handlePClick() can use "this" attributes even if its not an arrow function
+
+    this.state = {
+      name: 'Helo',
+      counter: 0
+    };
+  }*/
+  
+  state = {
+    name: 'helo',
+    counter: 0
+  }
+
+  handlePClick = () => {
+    // whenever state is chagned, render is called again
+    this.setState({name: 'tambara'});
+  } 
+
+  handleAClick = (event) => {
+    // arrow function doesn't need bind to use "this" attributes
+    event.preventDefault() // prevents clicking from taking us to the page with the link
+    const {counter} = this.state;
+    this.setState({counter: counter + 1});
+  }
+
+
+
   render() {
+    const { name, counter } = this.state;
+
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {1+1} - código Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handlePClick}>
+          {name} {counter}
         </p>
-        <a
+        <a onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
