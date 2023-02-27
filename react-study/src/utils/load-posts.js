13dd4d20383fb208f5cdfd9df896fa0c0a706961
Heloisa Tambara <heloisatambara@usp.br>
2023-02-27 12:03:36 -0300

    prettier

--- react-study/src/utils/load-posts.js
+++ react-study/src/utils/load-posts.js
@@ -1,6 +1,6 @@
 export const loadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
 
     const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
@@ -8,8 +8,8 @@ export const loadPosts = async () => {
     const photosJson = await photos.json();
 
     const postsAndPhotos = postsJson.map((post, index) => {
-      return {...post, cover: photosJson[index].url}
-    })
-    
+        return { ...post, cover: photosJson[index].url };
+    });
+
     return postsAndPhotos;
-} 
+};
