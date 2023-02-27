86f3a620757083cff8a6104498241de39dd0db35
Heloisa Tambara <heloisatambara@usp.br>
2023-02-27 12:00:44 -0300

    prettier

--- react-study/src/index.js
+++ react-study/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/global-styles.css';
-import { Home } from './templates/home';
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from "react-dom/client";
+import "./styles/global-styles.css";
+import { Home } from "./templates/home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Home />
+    </React.StrictMode>
 );
-
